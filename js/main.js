ddda11ca516cec42fb1c265eb8209253a3acc277
Daiane de Souza Alexandre <dsalexandre83@gmail.com>
2021-07-20 21:04:08 -0300

    Inclusão de conteúdo no HTML,exclusão e criação de classes no CSS,inserção do Javascript.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
-function myFunction(){
+/*function myFunction(){
     var x = document.getElementById("myDiv");
     if (x.style.display === "none") {
         x.style.display = "block";
     } else{
         x.style.display= "none";
     }
+}*/
+
+function togglePopup() {
+    document.getElementById("popup-1").classList.toggle("active");
 }
